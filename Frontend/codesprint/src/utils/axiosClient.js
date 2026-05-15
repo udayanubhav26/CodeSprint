7ac170455737e328a53a9c8c9d3650b644954547
Anubhav Sharma <anubhavsharma26114@gmail.com>
2026-05-15 17:08:18 +0530

    update API baseURL for production

--- Frontend/codesprint/src/utils/axiosClient.js
+++ Frontend/codesprint/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://codesprint-backend-p7wk.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
