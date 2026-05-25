a24a9278c62fb3d53c2eab92af27fdb067b50caf
Anubhav Sharma <anubhavsharma26114@gmail.com>
2026-05-25 17:58:30 +0530

    updated cors

--- Frontend/codesprint/src/Pages/About.jsx
+++ Frontend/codesprint/src/Pages/About.jsx
@@ -116,7 +116,7 @@ function About() {
                 <div>
                   <p className="text-gray-400 text-sm">Email</p>
                   <p className="text-white font-medium break-all">
-                    <a>codesprintsupport@gmail.com</a>
+                    <a href='mailto:codesprintsupport@gmail.com'>codesprintsupport@gmail.com</a>
                   </p>
                 </div>
               </div>
